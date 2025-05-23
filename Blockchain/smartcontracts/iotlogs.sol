f41040743f1e9d4feed74f172fbaf06092aa7eba
alehuuh <90169528+alehuuh@users.noreply.github.com>
2025-05-23 10:45:16 +0800

    Update iotlogs.sol

--- Blockchain/smartcontracts/iotlogs.sol
+++ Blockchain/smartcontracts/iotlogs.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract IotLogs {
     struct Iotlogrec {
+        uint transid;
         string deviceid;
         string devicename;
         string deviceloc;
@@ -11,27 +12,40 @@ contract IotLogs {
         string timestamp;
         string transvalue;
     } 
-    mapping (uint => Iotlogrec) Alliotlogs;
+    address owner;
 
-    function AddIotlogs(uint _transid, string memory _deviceid, string memory _devicename, string memory _deviceloc, string memory _rfid, string memory _classcode, string memory _timestamp, string memory _transvalue) public {
-        Alliotlogs[_transid].deviceid = _deviceid;
-        Alliotlogs[_transid].devicename = _devicename;
-        Alliotlogs[_transid].deviceloc = _deviceloc;
-        Alliotlogs[_transid].rfid = _rfid;
-        Alliotlogs[_transid].classcode = _classcode;
-        Alliotlogs[_transid].timestamp = _timestamp;
-        Alliotlogs[_transid].transvalue = _transvalue;
+    mapping (uint => Iotlogrec) public Alliotlogs;
+    Iotlogrec[] public IotlogrecArray;
 
+    constructor() {
+        owner = msg.sender;
     }
 
-    function getIotlogs(uint _transid) public view returns (string memory, string memory, string memory, string memory, string memory , string memory, string memory) {
-        return (Alliotlogs[_transid].deviceid,
-                 Alliotlogs[_transid].devicename,
-                 Alliotlogs[_transid].deviceloc,
-                 Alliotlogs[_transid].rfid,
-                 Alliotlogs[_transid].classcode,
-                 Alliotlogs[_transid].timestamp,
-                 Alliotlogs[_transid].transvalue);
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function AddIotlog(uint _transid, string memory _deviceid, string memory _devicename, string memory _deviceloc, string memory _rfid, string memory _classcode, string memory _timestamp, string memory _transvalue) public onlyOwner {
+        Iotlogrec memory IotLog = Iotlogrec(_transid, _deviceid, _devicename, _deviceloc, _rfid, _classcode, _timestamp, _transvalue);
+        Alliotlogs[_transid] = IotLog;
+        IotlogrecArray.push(Iotlogrec(_transid, _deviceid, _devicename, _deviceloc, _rfid, _classcode, _timestamp, _transvalue));
+    }
+
+    function getIotlog(uint _transid) public view returns (string memory, string memory, string memory, string memory, string memory , string memory, string memory) {
+        require(Alliotlogs[_transid].transid != 0, "Iot Logs not available.");
+        Iotlogrec memory Iotlog = Alliotlogs[_transid];
+        return (Iotlog.deviceid, 
+                 Iotlog.devicename,
+                 Iotlog.deviceloc,
+                 Iotlog.rfid,
+                 Iotlog.classcode,
+                 Iotlog.timestamp,
+                 Iotlog.transvalue);
+    }
+
+        function getAllIotlogs() public view returns (Iotlogrec[] memory) {
+            return IotlogrecArray;
     }
 
 }
