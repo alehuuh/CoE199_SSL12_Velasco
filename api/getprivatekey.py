ae909406e6b37592ae51379a702cc7f228cfee9d
alehuuh <90169528+alehuuh@users.noreply.github.com>
2025-05-23 10:43:51 +0800

    Update getprivatekey.py

--- api/getprivatekey.py
+++ api/getprivatekey.py
@@ -1,5 +1,5 @@
 from web3.auto import w3
-with open("C:/AlejaThesis/Blockchain/node1/data/keystore/UTC--2025-02-04T03-35-51.575507500Z--f0c0ba7bcbf04a504b3744bc57e725d6240cb9c5") as keyfile:
+with open("C:/-/Blockchain/node1/data/keystore/UTC--2025-02-04T03-35-51.575507500Z--f0c0ba7bcbf04a504b3744bc57e725d6240cb9c5") as keyfile:
     encrypted_key = keyfile.read()
     private_key = w3.eth.account.decrypt(encrypted_key, '123456')
 
